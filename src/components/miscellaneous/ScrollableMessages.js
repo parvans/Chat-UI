6574dca78bd645c9c3a6a30d9a6a70a16b16318e
parvans <parvansajeevan@gmail.com>
2023-07-10 17:19:25 +0530

    date ups

--- src/components/miscellaneous/ScrollableMessages.js
+++ src/components/miscellaneous/ScrollableMessages.js
@@ -8,87 +8,99 @@ import React from 'react'
 import ScrollableFeed from 'react-scrollable-feed'
 import moment from 'moment';
 import "./styles.css"
-
 export default function ScrollableMessages({messages}) {
     const userId=localStorage.getItem('auth-token')
     const uId=jwtDecode(userId)?.id;
+
+    const groupedDays = messages.reduce((groups, message) => {
+        const date = moment().diff(message.createdAt, 'days') === 0 ? 'Today' : moment(message.createdAt).format('DD/MM/YYYY');
+        // console.log(moment(message.createdAt).format('DD/MM/YYYY'));
+        if (!groups[date]) {
+            groups[date] = [];
+        }
+        groups[date].push(message);
+        return groups;
+    }, {});
+
+    // console.log(groupedDays);
+    const groupArrays = Object.keys(groupedDays).map((date) => {
+        return {
+            date,
+            messages: groupedDays[date]
+        };
+    });
+    console.log(groupArrays);
   return (
      <ScrollableFeed className='scroll-vard'>
         <>
         
         {
-            messages && messages.map((message,index)=>(
-                <div style={{display:'flex'}} key={message._id}>
-                    {/* {
-                        message.createdAt && (
-                            
-                            <small style={{color:"gray",fontSize:"10px", marginLeft:"5px",marginTop:"10px"}}>
-                                {moment(message.createdAt).fromNow()}
-                            </small>
-                        )
-                    } */}
+            groupArrays.map((group,index)=>(
+                <div key={index}>
+                    <div className="date" style={{textAlign:"center",marginTop:"10px",marginBottom:"10px",color:"white", fontWeight:"bold",fontSize:"20px"}}>
+                        {group.date}
+                    </div>
+                    <div className="messages">
+                        {
+           group.messages && group.messages.map((message,index)=>(
+            <div style={{display:'flex'}} key={message._id}>
 
-                    {( isSameSender(messages,message,index,uId)|| 
-                        isLastMessage(messages,index,uId)) &&(
-                        <Tooltip
-                        label={message.sender.name}
-                        placement='bottom-start'
-                        color={"white"}
-                        borderRadius={"10px"}
-                        backgroundColor={"black"}
-                        hasArrow>
-                            {/* <Avatar
-                            mt="7px"
-                            mr={1}
-                            size="sm"
-                            cursor="pointer"
-                            src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
-                            width="30px"
-                            height="30px"
-                            name={message.sender.name}
-                            /> */}
+                {( isSameSender(group.messages,message,index,uId)|| 
+                    isLastMessage(group.messages,index,uId)) &&(
+                    <Tooltip
+                    label={message.sender.name}
+                    placement='bottom-start'
+                    color={"white"}
+                    borderRadius={"10px"}
+                    backgroundColor={"black"}
+                    hasArrow>
 
-                            <div style={{
-                                backgroundSize:"cover",
-                                backgroundPosition:"center",
-                                width:"30px",
-                                height:"30px",
-                                borderRadius:"50%",
-                                marginTop:"10px",
-                                marginRight:"5px",
-                                cursor:"pointer",
-                                backgroundColor:`${message.sender._id===uId ? "#BEE3F8" : "#B9F5D0"}`,
-                                display:"flex",
-                                alignItems:"center",
-                                justifyContent:"center",
-                                fontWeight:"bold",
-                            }}>
-                                {message.sender.name[0]?.toUpperCase()}
-                            </div>
-                            
-                        </Tooltip>
+                        <div style={{
+                            backgroundSize:"cover",
+                            backgroundPosition:"center",
+                            width:"30px",
+                            height:"30px",
+                            borderRadius:"50%",
+                            marginTop:"10px",
+                            marginRight:"5px",
+                            cursor:"pointer",
+                            backgroundColor:`${message.sender._id===uId ? "#BEE3F8" : "#B9F5D0"}`,
+                            display:"flex",
+                            alignItems:"center",
+                            justifyContent:"center",
+                            fontWeight:"bold",
+                        }}>
+                            {message.sender.name[0]?.toUpperCase()}
+                        </div>
                         
-                    )}
+                    </Tooltip>
+                    
+                )}
 
-                    <span style={{
-                        backgroundColor:`${message.sender._id===uId ? "#BEE3F8" : "#B9F5D0"
-                    }`,
-                    borderRadius:"10px",
-                    padding:"5px 5px",
-                    maxWidth:"75%",
-                    marginLeft:isSameSenderMargin(messages,message,index,uId),
-                    marginTop:isSameUser(messages,message,index,uId)? 3 : 10,
-                }}>{
-                    message.content
-                }
-                < br/>
-                <small style={{color:"gray",fontSize:"10px", marginLeft:"5px"}}>
-                    {moment(message.createdAt).format('LT')}
-                </small>
-                </span>
-                
+                <span style={{
+                    backgroundColor:`${message.sender._id===uId ? "#BEE3F8" : "#B9F5D0"
+                }`,
+                borderRadius:"10px",
+                padding:"5px 5px",
+                maxWidth:"75%",
+                marginLeft:isSameSenderMargin(group.messages,message,index,uId),
+                marginTop:isSameUser(group.messages,message,index,uId)? 3 : 10,
+            }}>{
+                message.content
+            }
+            < br/>
+            <small style={{color:"gray",fontSize:"10px", marginLeft:"5px"}}>
+                {moment(message.createdAt).format('LT')}
+            </small>
+            </span>
+            
+            </div>
+            
+        ))
+
+        }
+                    </div>
                 </div>
-                
             ))
         }
         </>
--- src/views/ezhuth/Chat/ChatArea.js
+++ src/views/ezhuth/Chat/ChatArea.js
@@ -1,22 +1,16 @@
 import ChatBox from 'components/miscellaneous/ChatBox'
 import MyChats from 'components/miscellaneous/MyChats'
-import SideDrawer from 'components/miscellaneous/SideDrawer'
 import { ChatState } from 'context/ChatProvider'
 import React from 'react'
-import { Card, CardBody, CardFooter, CardHeader, CardTitle, Col, Row } from 'reactstrap'
 import DemoNavbar from "components/Navbars/DemoNavbar.js";
-import { Line,Pie } from 'react-chartjs-2'
-import { dashboard24HoursPerformanceChart } from 'variables/charts'
-import { dashboardEmailStatisticsChart } from 'variables/charts'
-import { dashboardNASDAQChart } from 'variables/charts'
+import { Row } from 'reactstrap';
 export default function ChatArea() {
-    const {user}=ChatState()
+    const {user,selectedChat, setSelectedChat}=ChatState()
     const [fetchAgain,setFetchAgain]=React.useState(false)
   return (
     <>
     <div className="content">
         <div style={{width:"100%"}}>
-            {/* {user&& <SideDrawer/>} */}
               <DemoNavbar/>
               <Row style={{display:"flex",justifyContent:"center",width:"100%",height:"91.5vh",padding:"10px",marginLeft:"0px",marginTop:"90px"}}>
                 {user&& <MyChats fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />}
@@ -28,65 +22,6 @@ export default function ChatArea() {
 
 
 
-
-          {/* <div className="content" style={{marginTop:"70px"}}>
-       
-        <Row>
-          <Col md="4">
-            <Card style={{backgroundColor:"#dee0df"}}>
-              <CardHeader>
-                <CardTitle tag="h5">Email Statistics</CardTitle>
-                <p className="card-category">Last Campaign Performance</p>
-              </CardHeader>
-              <CardBody style={{ height: "100%" }}>
-                <Pie
-                  data={dashboardEmailStatisticsChart.data}
-                  options={dashboardEmailStatisticsChart.options}
-                />
-              </CardBody>
-              <CardFooter>
-                <div className="legend">
-                  <i className="fa fa-circle text-primary" /> Opened{" "}
-                  <i className="fa fa-circle text-warning" /> Read{" "}
-                  <i className="fa fa-circle text-danger" /> Deleted{" "}
-                  <i className="fa fa-circle text-gray" /> Unopened
-                </div>
-                <hr />
-                <div className="stats">
-                  <i className="fa fa-calendar" /> Number of emails sent
-                </div>
-                
-              </CardFooter>
-            </Card>
-          </Col>
-          <Col md="8">
-            <Card className="card-chart" style={{backgroundColor:"#dee0df"}}>
-              <CardHeader>
-                <CardTitle tag="h5">NASDAQ: AAPL</CardTitle>
-                <p className="card-category">Line Chart with Points</p>
-              </CardHeader>
-              <CardBody>
-                <Line
-                  data={dashboardNASDAQChart.data}
-                  options={dashboardNASDAQChart.options}
-                  width={400}
-                  height={100}
-                />
-              </CardBody>
-              <CardFooter>
-                <div className="chart-legend">
-                  <i className="fa fa-circle text-info" /> Tesla Model S{" "}
-                  <i className="fa fa-circle text-warning" /> BMW 5 Series
-                </div>
-                <hr />
-                <div className="card-stats">
-                  <i className="fa fa-check" /> Data information certified
-                </div>
-              </CardFooter>
-            </Card>
-          </Col>
-        </Row>
-      </div> */}
 
     </>
   )
